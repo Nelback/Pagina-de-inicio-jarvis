1dd8157f8121e29f61c3f82c493c65af93f76aad
Nelback <96082876+Nelback@users.noreply.github.com>
2022-09-12 11:20:45 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const bton1 = document.getElementById("whatsapp")
+var irwhat ="https://web.whatsapp.com"
+function gowhat() {
+  (location.href=irwhat)}
+
+ bton1.addEventListener("click", gowhat)
+
+ const bton2 = document.getElementById("twitch")
+ var irtwi ="https://www.twitch.com"
+ function gotwi() {
+    (location.href=irwhat)
+    window.open(href=irtwi, "_blank")
+}
+ 
+  bton2.addEventListener("click", gotwi)
+
+  const bton3 = document.getElementById("youtube")
+ var iryou ="https://www.youtube.com"
+ function goyou() {
+    (location.href=irwhat)
+    window.open(href=iryou, "_blank")
+}
+ 
+  bton3.addEventListener("click", goyou)
